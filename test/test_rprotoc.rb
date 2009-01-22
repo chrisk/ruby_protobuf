c07bfe9bcd539a35fc8945cc219d3e4fb47394ac
Chris Kampmeier <chris@kampers.net>
2009-01-22 13:12:05 -0800

    Fix `rprotoc --version` to work with Jeweler's VERSION.yml

---
+++ test/test_rprotoc.rb
@@ -0,0 +1,11 @@
+require 'test/unit'
+
+class RprotocTest < Test::Unit::TestCase
+
+  def test_version_option
+    cmd = "#{File.dirname(__FILE__)}/../bin/rprotoc"
+    output = `#{cmd} -v`
+    assert_match /^rprotoc \d\.\d\.\d$/, output
+  end
+
+end
